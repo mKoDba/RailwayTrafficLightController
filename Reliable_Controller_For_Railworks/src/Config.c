39d71f295067635d29d65f7ca0032f476ed92736
sobakaAxolotl <joelmatosvaz@gmail.com>
2018-11-22 16:42:51 +0000

    This is an Hello World test commit
    Version 0
    
    TODO Everything about the project

---
+++ Reliable_Controller_For_Railworks/src/Config.c
@@ -0,0 +1,4 @@
+#define F_CPU 1000000UL
+#define setBit(sfr, bit)     (_SFR_BYTE(sfr) |= (1 << bit))
+#define clearBit(sfr, bit)   (_SFR_BYTE(sfr) &= ~(1 << bit))
+#define toggleBit(sfr, bit)  (_SFR_BYTE(sfr) ^= (1 << bit))
