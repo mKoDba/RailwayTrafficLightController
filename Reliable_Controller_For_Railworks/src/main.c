fb2b97daa37b49b334cfa24257fed19673e66ce2
Joel Vaz <joelmatosvaz@gmail.com>
2018-12-07 18:33:45 +0000

    Added comments to code.

--- Reliable_Controller_For_Railworks/src/main.c
+++ Reliable_Controller_For_Railworks/src/main.c
@@ -36,6 +36,8 @@ int main(void) {
     char input[7];			// added pointer?
 	
     while(1) {
+
+		//SERIAL COMMUNICATION CODE HERE
 		//Tests the serial communication
 		//if serial receive mode enabled do this code
         puts("Receive Mode enabled! \n");
@@ -60,27 +62,32 @@ int main(void) {
 			}
 			printf("\n \n");
 		}
+		//SERIAL COMMUNICATION CODE ENDS HERE
+		/////////////////////////////////////////////////////////////////////////////////////////////////
 
-		//uint8_t ret;
-		//ret = i2c_start(EXT_MEM);
+		//I2C COMMUNICATION CODE STARTS HERE
+		uint8_t ret;
+		ret = i2c_start(EXT_MEM);
 		/* check if failed to issue start condition, could be device not found*/
-		//if(ret){
-		//	i2c_stop();
+		if(ret){
+			i2c_stop();
 			/* print on terminal return value */
-		//	#ifdef DEBUG
-		//		printf("%d", ret);
-		//	#endif
-		//}
+			#ifdef DEBUG
+				printf("%d", ret);
+			#endif
+		}
 
-//		else {
-//			i2c_write(0x05);	/* write address = 0x05 */
-//			i2c_write(0x75);	/* write data to address 0x05 */
-//			ret = i2c_read_nack(); /* read one byte from address */
-//			i2c_stop();
-//			#ifdef DEBUG
-//				printf("%d", ret);
-//			#endif
-//		}
+		else {
+			i2c_write(0x05);	/* write address = 0x05 */
+			i2c_write(0x75);	/* write data to address 0x05 */
+			ret = i2c_read_nack(); /* read one byte from address */
+			i2c_stop();
+			#ifdef DEBUG
+				printf("%d", ret);
+			#endif
+		}
+		//I2C COMMUNICATION CODE STARTS HERE
+		/////////////////////////////////////////////////////////////////////////////////////////////////
     } //End While(1)
 	return 0; 
 }
