fc7526d84004a7a6c03767a1c553d3a7aa225cb1
Joel Vaz <joelmatosvaz@gmail.com>
2018-12-10 17:24:17 +0000

    Added new include file and new source file for light controller logic.

---
+++ Reliable_Controller_For_Railworks/inc/controller.h
@@ -0,0 +1,4 @@
+#include</home/sobaca/CommsForTrainLight/Reliable_Controller_For_Railworks/inc/config.h>
+
+#define Red_light PB0
+#define Green_light PB1
---
+++ Reliable_Controller_For_Railworks/src/controller.c
@@ -0,0 +1,49 @@
+/*********************************************************************************
+ *                                                                               *
+ *               EEPROM Memory Testing for ATMEGA328p                            *
+ *                                                                               *
+ *                                                                               *
+ *               Code guide: NASA C style guide 1994                             *
+ *                                                                               *
+ *               Authors: Joel Matos Vaz (joelmatosvaz@gmail.com)                *
+ *                        Mario Kodba (Mario's email (here)                      *
+ *               Institution: @Faculdade de Engenharia Universidade do Porto     *
+ *                                                                               *
+ ********************************************************************************/
+/*
+*
+*Code for controlling the lights
+*Emergence mode red and green both on and blinking
+*
+*/
+
+#include"/home/sobaca/CommsForTrainLight/Reliable_Controller_For_Railworks/inc/config.h"
+#include"/home/sobaca/CommsForTrainLight/Reliable_Controller_For_Railworks/inc/controller.h"
+
+void set_output_signals(void){
+    //function to set the output
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+
+void light_red(int state){
+    int RED_light=0;
+     RED_light = 0;
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+
+void light_green(int state){
+    int RED_light=0;
+     RED_light = 1;
+}
+
+//////////////////////////////////////////////////////////////////////////////////
+
+void emergence_mode(int state){
+    int RED_light=0;
+    int 
+     RED_light = 1;
+}
+
+//////////////////////////////////////////////////////////////////////////////////
