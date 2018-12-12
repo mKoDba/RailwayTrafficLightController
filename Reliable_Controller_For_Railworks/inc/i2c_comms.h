d2e08c074ca256149d12684072a089bc5c34f1f6
Mario <Mario@DESKTOP-12A3G8V>
2018-12-12 10:59:20 +0000

    added header functions

--- Reliable_Controller_For_Railworks/inc/i2c_comms.h
+++ Reliable_Controller_For_Railworks/inc/i2c_comms.h
@@ -1,11 +1,13 @@
-#include</home/sobaca/CommsForTrainLight/Reliable_Controller_For_Railworks/inc/config.h>
+//#include</home/sobaca/CommsForTrainLight/Reliable_Controller_For_Railworks/inc/config.h>
+#include "config.h"
 
-#define F_SCL 100000UL  /* define SCL freq - 100kHz or 400kHz according to datasheet */
-#define PRESCALER 1
-#define TWBR_VAL ((((F_CPU / F_SCL) / PRESCALER) - 16 )/2)
 
+void i2c_init(void);
 uint8_t i2c_start(uint8_t slave_address);
 void i2c_stop(void);
 
+uint8_t i2c_rep_start(uint8_t slave_address);
+void i2c_start_wait(uint8_t slave_address);
+uint8_t i2c_write(uint8_t data);
 uint8_t i2c_read_ack(void);
 uint8_t i2c_read_nack(void);
